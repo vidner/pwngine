0ec3e8a31dfda6038f12d3740d55f787ac8d0b27
Rendi Yuda Perkasa <44577776+vidner@users.noreply.github.com>
2020-02-19 17:48:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ echo "cd $cwd" >> pwngine
 echo "vagrant up  > /dev/null 2>&1" >> pwngine
 echo "vagrant ssh" >> pwngine
 chmod +x pwngine
-mv pwnigma /usr/local/bin
+mv pwngine /usr/local/bin
 vagrant up
